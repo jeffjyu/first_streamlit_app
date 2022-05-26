10fb3111e2ae93247e428452a279518536113452
jeffjyu <56454903+jeffjyu@users.noreply.github.com>
2022-05-26 15:27:53 -0600

    Update streamlit_app.py
    menu changed

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,11 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
+streamlit.header('❤Breakfast Menu❤')
 
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale. Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥞Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥐Kale. Spinach & Rocket Smoothie')
+streamlit.text('🍗Hard-Boiled Free-Range Egg')
+streamlit.text('🍵Avacade Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
